4d8d3d0078992037de61bf8813838488b1de8920
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-03 15:32:07 +0530

    feat: add private route component

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import { useAuth } from '../context';
+import { Navigate, Route } from 'react-router-dom';
+
+export default function PrivateRoute({ path, ...props }) {
+  const { authToken } = useAuth();
+
+  return authToken ? (
+    <Route {...props} />
+  ) : (
+    <Navigate to="/login" state={{ from: path }} />
+  );
+}
