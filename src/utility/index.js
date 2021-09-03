ed1c57837fe76ddd84148c07e871a0b6db6ba993
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-03 15:52:00 +0530

    refactor: index for utility dir

---
+++ src/utility/index.js
@@ -0,0 +1,5 @@
+import { loginValidationRules } from './validation/login';
+import { registerValidationRules } from './validation/registerUser';
+import { validate } from './validation/validate';
+
+export { loginValidationRules, registerValidationRules, validate };
