e17595e534976f8cd324edfcd830e886abb4d9d4
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-03 15:52:52 +0530

    refactor: index for pages dir

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './Home';
+import Watch from './Watch';
+import WatchVideo from './WatchVideo';
+import MyList from './MyList';
+import Playlist from './Playlist';
+import NotFound from './NotFound';
+import Login from './Forms/Login';
+import Signup from './Forms/Signup';
+
+export { Home, Watch, WatchVideo, MyList, NotFound, Login, Signup, Playlist };
