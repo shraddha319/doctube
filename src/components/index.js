d5943f3c46db6559af10b90a48f283fcd48434ac
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-03 15:38:02 +0530

    refactor: index for components dir

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Nav from './Nav';
+import DisplayCard from './DisplayCard';
+import VideoList from './VideoList';
+import Modal from './Modal';
+import AddToPlaylist from './Modal/AddToPlaylist';
+import EmptyList from './VideoList/EmptyList';
+import PrivateRoute from './PrivateRoute';
+
+export {
+  Nav,
+  DisplayCard,
+  VideoList,
+  Modal,
+  AddToPlaylist,
+  EmptyList,
+  PrivateRoute,
+};
