076296a796b46ddb034bac7e224423938bca0246
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-09 05:26:19 +0530

    feat: api calls; fetch, remove playlists

--- src/pages/Playlists/index.jsx
+++ src/pages/Playlists/index.jsx
@@ -1,10 +1,9 @@
 import './Playlists.scss';
 import { useData, useAuth } from '../../context';
 import { VideoList, Loader } from '../../components';
-import { lowerCaseHyphenate } from '../../utility/utils';
 import { LIKED_PL, WATCH_LATER_PL } from '../../utility/constants';
 import { useEffect, useState } from 'react';
-import { getPlaylists } from '../../api';
+import { getPlaylists, deletePlaylist } from '../../api';
 
 export default function Playlists() {
   const {
@@ -14,19 +13,35 @@ export default function Playlists() {
   const {
     auth: { user },
   } = useAuth();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState({
+    fetch: false,
+    remove: false,
+    removeId: null,
+  });
 
-  function removePlaylistHandler(playlistId) {
-    dispatchData({
-      type: 'REMOVE_PLAYLIST',
-      payload: { playlistId },
-    });
+  async function removePlaylistHandler(playlistId) {
+    try {
+      setLoading({ ...loading, remove: true, removeId: playlistId });
+      const { status } = await deletePlaylist(user._id, playlistId);
+      if (status === 204)
+        dispatchData({
+          type: 'REMOVE_PLAYLIST',
+          payload: { playlistId },
+        });
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response);
+      }
+      console.log(err);
+    } finally {
+      setLoading({ ...loading, remove: false, removeId: null });
+    }
   }
 
   useEffect(() => {
     (async () => {
-      if (playlists.length > 0) return;
-      setLoading(true);
+      if (playlists) return;
+      setLoading({ ...loading, fetch: true });
       try {
         const {
           data: {
@@ -36,39 +51,49 @@ export default function Playlists() {
         } = await getPlaylists(user._id);
 
         if (success)
-          dispatchData({ type: 'FETCH_PLAYLISTS', payload: { playlists } });
+          dispatchData({
+            type: 'FETCH_PLAYLISTS',
+            payload: {
+              playlists: playlists.map((pl) =>
+                (({ name, videos }) => ({ name, videos }))(pl)
+              ),
+            },
+          });
       } catch (error) {
         if (error.response) {
           console.log(error.response);
         }
         console.log(error);
       }
-      setLoading(false);
+      setLoading({ ...loading, fetch: false });
     })();
   }, []);
 
   return (
     <div className="Playlists layout--default">
-      {loading ? (
+      {loading.fetch || !playlists ? (
         <Loader />
       ) : (
         playlists.map((pl) => (
           <section className="playlist">
-            {![LIKED_PL, WATCH_LATER_PL].includes(pl.name) && (
-              <button
-                className="btn btn--icon btn--action"
-                onClick={() => removePlaylistHandler(pl._id)}
-              >
-                <span className="btn__icon fa--sm">
-                  <i class="fas fa-times"></i>
-                </span>
-              </button>
-            )}
+            {![LIKED_PL, WATCH_LATER_PL].includes(pl.name) &&
+              (loading.remove && loading.removeId === pl._id ? (
+                <Loader />
+              ) : (
+                <button
+                  className="btn btn--icon btn--action"
+                  onClick={() => removePlaylistHandler(pl._id)}
+                >
+                  <span className="btn__icon fa--sm">
+                    <i class="fas fa-times"></i>
+                  </span>
+                </button>
+              ))}
             <VideoList
               limit={5}
               videos={pl.videos}
               title={pl.name}
-              showAllLink={`${lowerCaseHyphenate(pl.name)}`}
+              showAllLink={`${pl._id}`}
             />
           </section>
         ))
