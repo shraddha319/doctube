1992307f4e1d40e819418fdc73bd45b1d8c39426
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-09 05:27:03 +0530

    feat: api call to fetch playlist on load, remove video

--- src/pages/Playlist/index.jsx
+++ src/pages/Playlist/index.jsx
@@ -1,64 +1,106 @@
 import './Playlist.scss';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useData } from '../../context';
-import { lowerCaseHyphenate } from '../../utility/utils';
-import { LIKED_PL, WATCH_LATER_PL } from '../../utility/constants';
-import { DisplayCard, EmptyList } from '../../components';
+import { useParams } from 'react-router-dom';
+import { useData, useAuth } from '../../context';
+import { DisplayCard, EmptyList, Loader } from '../../components';
+import { useState, useEffect } from 'react';
+import { updatePlaylist, getPlaylists } from '../../api';
 
 export default function Playlist() {
-  const { playlistName } = useParams();
-  const { data, dispatchData } = useData();
-  const playlist = data.playlists.find(
-    (pl) => lowerCaseHyphenate(pl.name) === playlistName
-  );
-  const navigate = useNavigate();
+  const { playlistId } = useParams();
+  const {
+    data: { playlists },
+    dispatchData,
+  } = useData();
+  const {
+    auth: { user },
+  } = useAuth();
+  const [loading, setLoading] = useState({
+    fetch: false,
+    remove: false,
+  });
+  const playlist = playlists?.find((pl) => pl._id === playlistId);
 
-  function removeFromPlaylistHandler(videoId) {
-    dispatchData({
-      type: 'REMOVE_FROM_PLAYLIST',
-      payload: { playlistName: playlist.name, videoId },
-    });
+  async function removeFromPlaylistHandler(videoId) {
+    try {
+      setLoading({ ...loading, remove: true });
+      const { status } = await updatePlaylist(user._id, playlistId, {
+        videos: [videoId],
+        type: 'remove',
+      });
+
+      if (status === 204)
+        dispatchData({
+          type: 'REMOVE_FROM_PLAYLIST',
+          payload: { playlistId, videoId },
+        });
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response);
+      }
+      console.log(err);
+    } finally {
+      setLoading({ ...loading, remove: false });
+    }
   }
 
-  function removePlaylistHandler() {
-    dispatchData({
-      type: 'REMOVE_PLAYLIST',
-      payload: { playlistId: playlist._id },
-    });
-    navigate('/my-list');
-  }
+  useEffect(() => {
+    (async () => {
+      if (playlists) return;
+      setLoading({ ...loading, fetch: true });
+      try {
+        const {
+          data: {
+            success,
+            data: { playlists },
+          },
+        } = await getPlaylists(user._id);
+
+        if (success)
+          dispatchData({
+            type: 'FETCH_PLAYLISTS',
+            payload: {
+              playlists: playlists.map((pl) =>
+                (({ name, videos }) => ({ name, videos }))(pl)
+              ),
+            },
+          });
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response);
+        }
+        console.log(error);
+      }
+      setLoading({ ...loading, fetch: false });
+    })();
+  }, []);
 
   return (
     <div className="layout--default playlist">
-      <header className="flex--row">
-        <h1 className="title title--h5">{playlist.name}</h1>
-        {![LIKED_PL, WATCH_LATER_PL].includes(playlist.name) && (
-          <button
-            className="btn btn--icon btn--action"
-            onClick={removePlaylistHandler}
-          >
-            <span className="btn__icon fa--sm">
-              <i class="far fa-trash-alt"></i>
-            </span>
-          </button>
-        )}
-      </header>
-      <section className="playlist__videos">
-        {playlist.videos.map((video) => (
-          <div className="playlist__video">
-            <DisplayCard video={video} />
-            <button
-              className="btn btn--icon btn--action"
-              onClick={() => removeFromPlaylistHandler(video._id)}
-            >
-              <span className="btn__icon fa--sm">
-                <i class="far fa-trash-alt"></i>
-              </span>
-            </button>
-          </div>
-        ))}
-        {playlist.videos.length === 0 && <EmptyList />}
-      </section>
+      {loading.fetch || !playlist ? (
+        <Loader />
+      ) : (
+        <>
+          <header className="flex--row">
+            <h1 className="title title--h5">{playlist.name}</h1>
+          </header>
+          <section className="playlist__videos">
+            {playlist.videos.map((video) => (
+              <div className="playlist__video">
+                <DisplayCard video={video} />
+                <button
+                  className="btn btn--icon btn--action"
+                  onClick={() => removeFromPlaylistHandler(video._id)}
+                >
+                  <span className="btn__icon fa--sm">
+                    <i class="far fa-trash-alt"></i>
+                  </span>
+                </button>
+              </div>
+            ))}
+            {playlist.videos.length === 0 && <EmptyList />}
+          </section>
+        </>
+      )}
     </div>
   );
 }
