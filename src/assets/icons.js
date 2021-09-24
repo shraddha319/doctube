e045a96d490143b2b2ff432e9874cba5243e1cd0
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-24 13:26:21 +0530

    feat: add react-icons

---
+++ src/assets/icons.js
@@ -0,0 +1,31 @@
+import {
+  IoHeartOutline as HeartOutline,
+  IoHeartSharp as HeartSolid,
+  IoTrashBinOutline as TrashBinIcon,
+} from 'react-icons/io5';
+
+import {
+  MdWatchLater as WatchLater,
+  MdPlaylistPlay as PlaylistIcon,
+} from 'react-icons/md';
+
+import { CgPlayListRemove as RemovePlaylistIcon } from 'react-icons/cg';
+
+import {
+  AiFillClockCircle as ClockSolid,
+  AiOutlineClockCircle as ClockOutline,
+} from 'react-icons/ai';
+
+import { RiPlayListAddLine as AddPlaylistIcon } from 'react-icons/ri';
+
+export {
+  HeartOutline,
+  HeartSolid,
+  WatchLater,
+  RemovePlaylistIcon,
+  TrashBinIcon,
+  ClockSolid,
+  ClockOutline,
+  AddPlaylistIcon,
+  PlaylistIcon,
+};
