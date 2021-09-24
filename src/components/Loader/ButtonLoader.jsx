9147df68df220e016ceac0b4787039a189fc15d8
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-24 13:28:07 +0530

    feat: add button loader component

---
+++ src/components/Loader/ButtonLoader.jsx
@@ -0,0 +1,9 @@
+export default function ButtonLoader() {
+  return (
+    <button className="btn btn--disabled btn--icon">
+      <span class="fa--sm fa--primary">
+        <i class="fa fa-spinner fa-spin"></i>
+      </span>
+    </button>
+  );
+}
